f445acc58e3a8051e08d157725a4967d6ae62df0
trolihoctapcovan <nguyenvan1.c2lh@gmail.com>
2026-01-23 22:44:20 +0700

    Update sheetService.ts

--- src/services/sheetService.ts
+++ src/services/sheetService.ts
@@ -21,7 +21,7 @@ import {
 // ==================== CONFIGURATION ====================
 
 // 🔴 QUAN TRỌNG: Thay URL này bằng URL Web App của bạn sau khi deploy code.gs
-export const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwNvgZq2ISGXd_S232RdfrJiAnY1h-W-q7amjk6Jgw9mjsq22mA2kCmV9pRiiKxBYjkdQ/exec';
+export const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzQXomfhGx-vtdUSSbj7B-twMvSZI3hNXevZIV1OGjz6rfWttgpaUMbWvvJfvgZBTmHag/exec';
 
 // API Response interface
 interface APIResponse<T = unknown> {
